b9e8f880cd2a594cd32d32f7ae8a1fa043c2f862
luzhihuan <ym2350542910@163.com>
2025-02-09 14:25:23 +0800

    加入login.vue

---
+++ easycom/easycom-front/src/router/index.js
@@ -0,0 +1,35 @@
+import {createRouter,createWebHistory} from "vue-router";
+
+import VueCookies from "vue-cookies"
+
+const router =createRouter({
+    history:createWebHistory(import.meta.env.BASE_URL),
+    routes:[
+        {
+            path:'/login',
+            name:'/login',
+            component:()=>import("@/views/Login.vue")
+        },
+        {
+            path:'/',
+            name:'Framework',
+            component:()=>import("@/views/Framwork.vue")
+        },
+    ]
+})
+router.beforeEach((to,from,next,)=>{
+    const userInfo = VueCookies.get("userInfo")
+    if(to.meta.needLogin!=null&&to.meta.needLogin&&userInfo==null){
+        router.push("/login")
+    }
+    next()
+})
+
+export default router
+
+
+
+
+
+
+
