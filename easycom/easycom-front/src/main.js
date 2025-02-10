9ea196b1d92dbb7b6141463940c4ef48b6990935
luzhihuan <ym2350542910@163.com>
2025-02-10 13:45:07 +0800

    修改部分文件

--- easycom/easycom-front/src/main.js
+++ easycom/easycom-front/src/main.js
@@ -7,7 +7,7 @@ import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 import '@/assets/base.scss'
-
+import '@/assets/icon/iconfont.css'
 
 
 const app = createApp(App)
